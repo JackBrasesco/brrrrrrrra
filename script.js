3cb666457f84ac87ea5ba8d541eac65e5174961c
JackBrasesco <jacbras@nuevaschool.org>
2016-10-17 08:48:49 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 var person = {
   name: "David",
-  representation: "http://i.dailymail.co.uk/i/pix/2016/03/14/19/322C3FA100000578-0-image-a-13_1457982438669.jpg",
+  representation:  "http://i.dailymail.co.uk/i/pix/2016/03/14/19/322C3FA100000578-0-image-a-13_1457982438669.jpg",
   height: "500px",	
   favoriteColor: "blue",
   catchphrase: "Get pumped!"
 }
 
 var display = $("#display");
+var display2 = $("#display2");
+
 var name = person.name;
 var picture = person.representation;
 var catc = person.catchphrase;
 
 function drawPerson(person) {
   display.html(name)
-//  display.html(picture)
+  display.html(picture)
 //  display.html(catc)
 }
 
